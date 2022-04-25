12455d56bd17e13b20a71c2d1aac2c46b93f1ca4
Luqman <luqmant51@gmail.com>
2022-04-25 11:10:28 +0500

    node module

---
+++ day25_4_2022/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    console.log("I am route")
+})
+app.post('/user',(req,res)=>{
+    console.log(req.body)
+    console.log(req.body.user)
+})
+
+
+
+app.listen(3000,
+    console.log("Server is listen on  http://localhost:3000")
+)
