5e24ea37279275d766dd37319a58c20752c941f8
Luqman <luqmant51@gmail.com>
2022-04-27 15:51:42 +0500

    bcrypt

--- day27_4_2022/index.js
+++ day27_4_2022/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const mysql = require('mysql');
-const { log } = require('console');
+const brypt = require('bcrypt')
+
+const saltRounds = 10;
 
 app.use(cors())
 app.use(express.json())
@@ -22,6 +24,46 @@ db.connect((error) => {
     }
 })
 
+app.get('/getuser', (req, res) => {
+    const id = req.params.id
+    db.query(`SELECT * FROM users`, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(result)
+        }
+    })
+})
+
+
+app.post('/signup', async (req, res) => {
+    console.log(`We are in sigun up function`)
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const cpassword = req.body.cpassword
+    const encPass = await brypt.hash(password, 13);
+    const cEncPass = await brypt.hash(password, 13);
+    console.log(encPass)
+    console.log(cEncPass)
+    db.query(`INSERT INTO users (name, email, password,cpassword) VALUES ('${name}','${email}','${encPass}','${cEncPass}')`,
+        (error, result) => {
+            if (error) {
+                console.log(error)
+            }
+            else {
+                console.log('The user is ', result)
+            }
+        })
+    res.status(200).json({
+        name,
+        email,
+        password,
+        cpassword,
+    })
+})
+
 app.post('/signin', (req, res) => {
     console.log(`We are in signin function`)
     const email = req.body.email
@@ -29,27 +71,6 @@ app.post('/signin', (req, res) => {
     res.status(200).json({
         email,
         password,
-    })
-})
-app.post('/signup', (req, res) => {
-    console.log(`We are in sigun up function`)
-    const name = req.body.name
-    const email = req.body.email
-    const password = req.body.password
-    const cpassword = req.body.cpassword
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(cpassword);
-    db.query(`INSERT INTO users (name, email, password,cpassword) VALUES ('${name}','${email}','${password}','${cpassword}')`,
-        (error, result, fields) => {
-            console.log('The user is ', result)
-        })
-    res.status(200).json({
-        name,
-        email,
-        password,
-        cpassword,
     })
 })
 
