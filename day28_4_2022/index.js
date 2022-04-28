c9634dab86756b43f12f3733544fbbf9e81aa4f6
Luqman <luqmant51@gmail.com>
2022-04-28 14:09:14 +0500

    sequallizer

--- day28_4_2022/index.js
+++ day28_4_2022/index.js
@@ -5,11 +5,12 @@ const bcrypt = require('bcrypt')
 const { Sequelize, DataTypes, Model } = require('sequelize')
 const sequelize = new Sequelize('node', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
-}, {
-    freezeTableName: true,
-    timestamps: false
+    dialect: 'mysql',
+    define: {
+        freezeTableName: true,
+        timestamps: false
 
+    }
 });
 
 async function connected() {
@@ -34,6 +35,25 @@ app.get('/', async (req, res) => {
     const getAllUser = await Users.findAll()
     console.log(getAllUser)
 })
+app.get('/email', async (req, res) => {
+    // Model.findAll()
+    const getAllUser = await Users.findAll({ attributes: ['email'] })
+    console.log(getAllUser[0])
+})
+
+app.post('/signup', async (req, res) => {
+    console.log(`We are in sigun up function`)
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const cpassword = req.body.cpassword
+    const addUser = await Users.create({ name, email, password, cpassword })
+    console.log(addUser)
+    console.log(name)
+    console.log(email)
+    console.log(password)
+})
+
 
 app.listen(3000,
     console.log("Server is listen on  http://localhost:3000")
